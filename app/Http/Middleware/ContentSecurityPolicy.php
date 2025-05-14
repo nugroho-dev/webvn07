0d44e32402d4d3b78166236166bd7bfa6b679adb
nugroho-dev <didik.ngr@gmail.com>
2025-05-14 07:42:15 +0700

    update link csp

--- app/Http/Middleware/ContentSecurityPolicy.php
+++ app/Http/Middleware/ContentSecurityPolicy.php
@@ -32,9 +32,9 @@ class ContentSecurityPolicy
         $response->headers->set('Content-Security-Policy', implode('; ', [
             "default-src 'none'",
             "font-src 'self' https://rsms.me data:",
-            "script-src 'self' 'nonce-{$nonce}' https://code.jquery.com https://cdn2.woxo.tech https://platform.twitter.com/ https://www.instagram.com/",
+            "script-src 'self' 'nonce-{$nonce}' https://code.jquery.com https://cdn2.woxo.tech https://platform.twitter.com/ https://www.instagram.com/ https://cdnjs.cloudflare.com/ https://buttons.github.io/",
             "connect-src 'self' https://www.google-analytics.com https://analytics.google.com",
-            "style-src 'self' 'unsafe-inline' https://rsms.me/ https://code.jquery.com",
+            "style-src 'self' 'unsafe-inline' https://rsms.me/ https://code.jquery.com https://fonts.googleapis.com/",
             "img-src 'self' data: https://www.google-analytics.com https://www.google.co.id",
             "frame-src https://verifikasipdf.rootca.id/ https://www.google.com/ https://widgets.woxo.tech/ https://www.facebook.com/ https://platform.twitter.com https://syndication.twitter.com/ https://www.instagram.com https://www.youtube.com",
             "base-uri 'self'",
